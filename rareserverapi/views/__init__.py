445a9003dc97db006c97bd8d9f1fa80b02c140fd
Nicholas Casale <nicholas.e.casale@gmail.com>
2022-11-17 13:44:02 -0600

    Subsctiptions have CRUD functionality

--- rareserverapi/views/__init__.py
+++ rareserverapi/views/__init__.py
@@ -4,4 +4,4 @@ from .category_view import CategoryView
 from .comment_view import CommentView
 from .auth import register_user, login_user
 from .tag_view import TagView
-
+from .subscription_view import SubscriptionView
---
+++ rareserverapi/views/subscription_view.py
@@ -0,0 +1,50 @@
+"""View module for handling requests about game types"""
+from tkinter import E
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from rareserverapi.models import Subscription, Member
+
+
+class SubscriptionView(ViewSet):
+    """Level up game types view"""
+
+    def retrieve(self, request, pk):
+        subscription = Subscription.objects.get(pk=pk)
+        serializer = SubscriptionSerializer(subscription)
+        return Response(serializer.data)
+
+    def list(self, request):
+        subscription = Subscription.objects.all()
+        serializer = SubscriptionSerializer(subscription, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized game instance
+        """
+        follower = Member.objects.get(user=request.auth.user)
+        author = Member.objects.get(pk=request.data["author"])
+
+        subscription = Subscription.objects.create(
+            follower=follower,
+            author=author,
+            created=request.data["created"]
+        )
+        serializer = SubscriptionSerializer(subscription)
+        return Response(serializer.data)
+
+    def destroy(self, request, pk):
+        subscription = Subscription.objects.get(pk=pk)
+        subscription.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Subscription
+        fields = ('id', 'follower', 'author', 'created')
